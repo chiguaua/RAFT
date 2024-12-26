440e79085bfda762a974938e4ce7672a3afad9e6
chiguaua <91546000+chiguaua@users.noreply.github.com>
2024-12-27 06:00:45 +0700

    псевдо таймер...

--- server1 copy.py
+++ server1 copy.py
@@ -9,6 +9,7 @@ import fastapi_jsonrpc as jsonrpc
 import jinja2
 import io
 import base64
+import threading
 from threading import Timer
 from datetime import datetime, timedelta
 import time
@@ -28,13 +29,14 @@ ping_timeout = 0.2
 vote_timeout = 0.2
 action_timeout = 0.1
 hb_timer = 0.3
-drop_timeout = 2
-sleep_max_time = 0.6
+drop_timeout = 3
+sleep_max_time = 2
 
 class RepeatTimer(Timer):
     def run(self):
         while not self.finished.wait(self.interval):
             self.function(*self.args, **self.kwargs)
+
 class MyError(jsonrpc.BaseError):
     CODE = 5000
     MESSAGE = 'My error'
@@ -47,10 +49,14 @@ class AddNodeIn(BaseModel):
 class AddNodeOut(BaseModel):
     leader: int
 
-class VotingIn(BaseModel):
+class VoteIn(BaseModel):
     term: int
-    port: int
-
+    candidateId: int
+    lastLogIndex: int
+    lastLogTerm: int
+class VoteOut(BaseModel):
+    term: int
+    voteGranted: bool
 class AppendEntriesIn(BaseModel):
     term: int #term leader’sterm 
     leaderId: int # sofollowercanredirectclients
@@ -58,7 +64,6 @@ class AppendEntriesIn(BaseModel):
     prevLogTerm: int # term of prev Log Indexentry
     entries: list[tuple[str, int, int]] #logentriestostore(emptyforheartbeat; maysendmorethanoneforefficiency)
     leaderCommit: int #leader’s commitIndex
-
 class AppendEntriesOut(BaseModel):
     term: int# currentTerm,forleadertoupdateitself
     success: bool# trueiffollowercontainedentrymatching prevLogIndexandprevLogTerm
@@ -81,15 +86,20 @@ class MySyncObj():
 
     nodes : Dict[int, Any] = {}#, 8016:0} # 8010
     vote_start = datetime.now()
-    already_sleep = False
-    last_ping = datetime.now()
+    vote_state = "drop" # drop -> sleep -> candidat/vote
     #HB
     steps = (drop_timeout//hb_timer)//3
     curr_step = 0
 
+    def wait(self, time = drop_timeout):
+        self.riot.cancel()
+        self.riot = Timer(time, self.do_work)
+        self.riot.start()
+
     def __init__(self):
-        self.timer = RepeatTimer(action_timeout, self.do_work)
         self.heartbeat = RepeatTimer(hb_timer, self.hearthbit)
+        self.riot = Timer(drop_timeout, self.wait)
+        
         print("connect")
         asyncio.run(self.connect_to_net(self.leader_port))
 
@@ -121,7 +131,7 @@ class MySyncObj():
         elif (leader_get == port):
             print("Succ connect ", port)
             self.leader_port = port
-            self.last_ping = datetime.now()
+            self.wait(drop_timeout)
         else:
             self.connect_to_net(leader_get)
            
@@ -186,7 +196,6 @@ class MySyncObj():
         if (self.nodes[node] + timedelta(seconds=drop_timeout) < datetime.now()):
             self.new_log("Drop", node, self.term)
         
-    
     def hearthbit(self):
         # nodes_list = list(self.nodes.keys())
         # nodes_hb = nodes_list[int(len(nodes_list)*self.curr_step//self.steps) : int(len(nodes_list)*(self.curr_step + 1)//self.steps)]
@@ -211,33 +220,35 @@ class MySyncObj():
             "params": {
                 "in_params": {
                     "term": self.term,
-                    "port": self.cur_port
+                    "candidateId": self.cur_port,
+                    "lastLogIndex": self.commitIndex,
+                    "lastLogTerm": self.log[self.commitIndex-1][1]
                 }
             }
         }
 
         try:
             response = requests.post(url, json=data, headers=headers, timeout=vote_timeout)
-            print(f"{node} vote responded {response.json().get('result')}")
-            if response.status_code == 200 and response.json().get("result"):
+            print(f"{node} vote responded {response.json()['result']['voteGranted']}")
+            if response.status_code == 200 and response.json()['result']['voteGranted']:
                 return 1
+            if response.status_code == 200 and response.json()['result']['term'] > self.term:
+                self.role = "follower"
+                self.term = response.json()['result']['term']
+                return 0
         except requests.exceptions.RequestException as e:
             print(f"{node} vote failed")
         return 0  
     
     async def coronation(self):
-        self.already_sleep = True
-        sleep_duration = random.uniform(0, sleep_max_time) 
-        print("sleep ", sleep_duration*1000//1)
-        await asyncio.sleep(sleep_duration)
-        if (self.vote_for == None and self.last_ping + timedelta(seconds=drop_timeout) <= datetime.now()) :
-            print("try to lead")
-            self.role = "candidat"
+        if (self.vote_for == self.cur_port) :
             votes = 1
             self.term = self.term + 1
             # self.send_vote_request(8016)
+            print(datetime.now())
             tasks = [asyncio.create_task(self.send_vote_request(node)) for node in self.nodes if node != self.cur_port]
             results = await asyncio.gather(*tasks, return_exceptions=True)
+            print(datetime.now())
             votes = sum(results) + 1
             print(f"Election finished\n{votes}/{len(tasks)}")
 
@@ -256,25 +267,38 @@ class MySyncObj():
                 print("folower...")
                 self.role = "follower"
         
-        self.already_sleep = False
-        
-    def do_work(self):
-        # print(f"hello im {self.cur_port} ({self.leader_port}), {self.role}, {self.term}")
-          
+    def do_work(self):        
         if self.role == "follower":
-            if  self.vote_start + timedelta(seconds=vote_timeout) <= datetime.now() and self.vote_for != None:
-                print("end vote round")
+            if self.vote_state == "drop":
+                sleep_duration = random.uniform(0, sleep_max_time) 
+                print("leader Drop. sleep ", sleep_duration*1000//1, datetime.now())
+                self.vote_state = "sleep"
                 self.vote_for = None
-            if self.last_ping + timedelta(seconds=drop_timeout) <= datetime.now() and self.vote_for == None and not self.already_sleep:
-                print("leader Drop ", datetime.now())
-                # asyncio.run(self.coronation())
+                self.wait(sleep_duration)
+            elif self.vote_state == "sleep" and self.vote_for == None:
+                print("try to lead", datetime.now())
+                self.role = "candidat"
+                self.vote_state = "drop"
+                self.vote_for = self.cur_port
+                asyncio.run(self.coronation())
+                self.wait(vote_timeout)
+            else:
+                sleep_duration = random.uniform(0, sleep_max_time) 
+                print("end_vote. sleep ", sleep_duration*1000//1)
+                self.vote_state = "sleep"
+                self.vote_for = None
+                self.wait(sleep_duration)
+        if self.role == "candidat":
+            self.role = "follower"
+            print("WTF")
+        self.wait()
 
     def start(self):
-        self.timer.start()
-    
+        self.wait(drop_timeout)
+
     def stop(self):
-        self.timer.cancel()
         self.heartbeat.cancel()
+        self.riot.cancel()
 
 @api_v1.method(errors=[MyError])
 async def add_to_list(in_params: AddNodeIn) -> AddNodeOut:
@@ -289,7 +313,10 @@ async def append(in_params: AppendEntriesIn) -> AppendEntriesOut:
     # print("app get", my_raft.log, in_params)
     if(my_raft.term > in_params.term):
         return AppendEntriesOut(term= my_raft.term, success= False)
-    my_raft.last_ping = datetime.now()
+    
+    my_raft.wait(drop_timeout)
+    my_raft.vote_state = "drop"
+
     if(my_raft.role != "follower"):
         print("WTF")
         my_raft.role = "follower"
@@ -300,7 +327,7 @@ async def append(in_params: AppendEntriesIn) -> AppendEntriesOut:
        (my_raft.commitIndex == in_params.prevLogIndex and my_raft.log[in_params.prevLogIndex - 1][2] != in_params.prevLogTerm)):
         return AppendEntriesOut(term= my_raft.term, success= False) 
     
-
+    
     my_raft.leader_port = in_params.leaderId
     my_raft.log = my_raft.log[:in_params.prevLogIndex+1]
     for x in in_params.entries:
@@ -312,14 +339,15 @@ async def append(in_params: AppendEntriesIn) -> AppendEntriesOut:
     return AppendEntriesOut(term = my_raft.term, success= True) 
 
 @api_v1.method(errors=[MyError])
-async def vote(in_params: VotingIn) -> bool:
-    if my_raft.role == "follower" and my_raft.term < in_params.term and my_raft.vote_for == None and my_raft.last_ping + timedelta(seconds=drop_timeout) <= datetime.now():
-        my_raft.vote_for = in_params.port
-        my_raft.vote_start = datetime.now()
-        print("request vote for ", in_params.port)
-        return True
-    print("Denied", in_params.port)#, my_raft.role == "follower", my_raft.term <= in_params.term, my_raft.vote_for == None, my_raft.last_ping + timedelta(drop_timeout) <= datetime.now(), datetime.now())
-    return False
+async def vote(in_params: VoteIn) -> VoteOut:
+    if my_raft.role == "follower" and my_raft.term <= in_params.term and my_raft.vote_for == None and my_raft.vote_state == "sleep":
+        my_raft.vote_for = in_params.candidateId
+        my_raft.wait(vote_timeout)
+        my_raft.term = in_params.term
+        print("request vote for ", in_params.candidateId)
+        return VoteOut(term= my_raft.term, voteGranted= True)
+    print("Denied", in_params.candidateId, datetime.now())#, my_raft.role == "follower", my_raft.term <= in_params.term, my_raft.vote_for == None, my_raft.last_ping + timedelta(drop_timeout) <= datetime.now(), datetime.now())
+    return VoteOut(term= my_raft.term, voteGranted= False)
 
 def signal_handler(sig, frame):
     my_raft.stop()
